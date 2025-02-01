6cf60da1545658591a817ecb3dbd680c5c3a68d2
Lovnyth Valcin <lovnyth@gmail.com>
2025-02-01 00:14:59 +0000

    arrays adds in js file

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import "bootstrap";
 import "./styles.css";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["My Dog", "My Phone", "My Tire", "You"];
+  let action = ["Ate", "Battery", "Is", "Called Me"];
+  let what = ["My Homework", "Died", "Flat", "Too Late"];
+
+  let myExcuse = who.concat(action, what);
+  document.getElementById("excuse").innerHTML = myExcuse;
 };
