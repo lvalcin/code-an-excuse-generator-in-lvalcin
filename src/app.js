2742ff7b3775f3ed62a9f05bb8e2f2b8b3bae94a
Lovnyth Valcin <lovnyth@gmail.com>
2025-02-01 02:11:05 +0000

    math.random function applied

--- src/app.js
+++ src/app.js
@@ -7,6 +7,12 @@ window.onload = function() {
   let action = ["Ate", "Battery", "Is", "Called Me"];
   let what = ["My Homework", "Died", "Flat", "Too Late"];
 
-  let myExcuse = who.concat(action, what);
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+
+  let myExcuse =
+    who[whoIndex] + " " + action[actionIndex] + " " + what[whatIndex];
+
   document.getElementById("excuse").innerHTML = myExcuse;
 };
